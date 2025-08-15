2077d72c4d2197c25d21028fa254cf41ec6d469a
Daniel Park <intheimageofyeshua@gmail.com>
2025-08-15 19:42:56 +0900

    HTTP Version with Test

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -5,12 +5,28 @@ pub enum Method {
     Uninitialized,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+
 impl From<&str> for Method {
     fn from(s: &str) -> Method {
         match s {
             "GET" => Method::Get,
             "POST" => Method::Post,
             _ => Method::Uninitialized,
+        }
+    }
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Version {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
         }
     }
 }
@@ -22,5 +38,11 @@ mod tests {
     fn test_method_into() {
         let m: Method = "GET".into();
         assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let m: Version = "HTTP/1.1".into();
+        assert_eq!(m, Version::V1_1);
     }
 }
